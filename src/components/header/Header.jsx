eef7869711e244f1798a85c71e1ed40ea7b15970
Sudip Kundu <sudipkundu999@gmail.com>
2022-05-17 19:19:05 +0530

    feat : search functionality

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,35 @@
-import { Link } from "react-router-dom";
-import { useAuth } from "../../contexts";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth, useQuiz } from "../../contexts";
+import Select from "react-select";
 import "./header.css";
 
 export const Header = () => {
   const { isUserLoggedIn, userName, logoutHandler } = useAuth();
+  const navigate = useNavigate();
+  const { quizNamesFromDB } = useQuiz();
+  const searchOptions = quizNamesFromDB.map((item) => ({
+    ...item,
+    label: item.name,
+  }));
+
   return (
     <header>
       <div className="navigation-container">
         <Link to="/">
           <h2>CobraQuiz</h2>
         </Link>
+        <div className="nav-search search-wrapper">
+          <Select
+            options={searchOptions}
+            isClearable="true"
+            placeholder="Search"
+            onChange={(opt) =>
+              opt &&
+              navigate(`/${opt?.category}/${opt?._id}`, { replace: true })
+            }
+          />
+        </div>
+
         <div className="nav-right">
           {isUserLoggedIn && (
             <button className="btn btn-secondary" onClick={logoutHandler}>
